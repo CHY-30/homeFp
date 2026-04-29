d0c655f1a08d115675266e8ec2c9cca0eb7f2863
CHY-30 <choshoo82@gmail.com>
2026-04-29 18:58:30 +0900

    zustand_New
    로그인 체크 컴퍼넌트 및 레이아웃
    좌측에 테스트

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,6 +1,10 @@
 import { Outlet, Link } from "react-router-dom";
+import useAuthStore from "../store/useAuthStore";
 
 export default function Layout() {
+
+  const { userId, userName, isLoggedIn } = useAuthStore();
+
   return (
     <div className="wrap">
 
@@ -13,7 +17,14 @@ export default function Layout() {
       <div className="middle">
         <aside className="sidebar">
           <div> 
-            <div><Link to="/Login">로그인하기</Link></div>
+            {isLoggedIn === 1 ? (
+              <>
+              <div>{userName}({userId})</div>
+              <div><Link to="/Login">로그아웃하기</Link></div>
+              </>
+            ) : (
+              <div><Link to="/Login">로그인하기</Link></div>
+            )}
             <div><Link to="/BoardList">게시판으로</Link></div>
           </div>
         </aside>
---
+++ src/store/useAuthPage.tsx
@@ -0,0 +1,19 @@
+import { Navigate } from 'react-router-dom';
+import useAuthStore from '../store/useAuthStore';
+import type { ReactNode } from 'react';
+
+interface authIf{
+    children?: ReactNode;
+}
+
+const AuthGuard = ({ children }: authIf) => {
+  const { isLoggedIn } = useAuthStore();
+
+  if (isLoggedIn === 0) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default AuthGuard;
